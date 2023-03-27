48b1142b50595a05cd49cbbd004247c8a40b6573
scouca <scouca@github.com>
2023-03-27 23:15:32 +0200

    user page adjustment :)

--- react_app/src/pages/Profile.tsx
+++ react_app/src/pages/Profile.tsx
@@ -1,18 +1,21 @@
+/*
 import React from "react";
 import {getCurrentUser} from "../services/auth.service";
-import {Link} from "react-router-dom";
-import st from '../styles/Profile.module.css';
+import { Link } from "react-router-dom";
+import * as st from '../styles/tailwind/ProfileUser.style';
+/* import st from '../styles/Profile.module.css'; */
+/*} 
 
 const Profile: React.FC = () => {
     const currentUser = getCurrentUser();
     return (
-        <div className={st.container}>
+        <div className={st.Profile.box}>
 
             <header>
 
                 <h1> Welcome <strong>{currentUser.username}</strong></h1>
 
-                <img className={st.profileImg} alt="bob" src={require('../../src/styles/images/bob.jpg')}/>
+                <img className={st.Profile.imag} alt="avatar" src={require('../../src/styles/images/user.jpg')} />
 
             </header>
             <p>
@@ -28,8 +31,65 @@ const Profile: React.FC = () => {
         </div>
 
     );
-};
+}; 
+
+export default Profile; 
+
+ */
+import React from "react";
+import { getCurrentUser } from "../services/auth.service";
+import { Link } from "react-router-dom";
+import * as st from '../styles/tailwind/ProfileUser.style';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button 
+} from "@material-tailwind/react";
+
+const Profile: React.FC = () => {
+    const currentUser = getCurrentUser();
+
+    return (
+        < >
+            <Card className="container shadow-lg p-32" >
+                <CardHeader floated={false} className="flex justify-center" color="transparent" >
+                <img
+                        src="../user.jpg"
+                        alt="avatar"
+                        className={st.Profile.imag}
+                />
+           
+                </CardHeader>
+
+            <CardBody>
+                <div className="mb-3 flexjustif y-between ">
+                    <Typography variant="h3" color="blue-gray"  >
+                        Welcome <strong >{currentUser.username}</strong>
+                    </Typography>
+                   
+                </div>
+                <Typography color="gray">
+                    <p>
+                        <strong>Token:&nbsp;</strong> {JSON.stringify(currentUser.token).substring(16, 50)} ...
+                    </p>
+                    <p>
+                        <strong>Email:&nbsp;</strong> {currentUser.email}
+                    </p>
+                    <strong>Authorities: &nbsp;</strong>{currentUser.role}
+                </Typography>
+                </CardBody>
+
+                <CardFooter className="pt-3">
+                    <Button className="hover:scale-125" >
+                    <Link to="/home" className="text-white">Back to Home Page</Link>
+                </Button>
+            </CardFooter>
+             </Card>
+        </>
+   );
+ }
 
 export default Profile;
-
-
---
+++ react_app/src/styles/tailwind/ProfileUser.style.js
@@ -0,0 +1,8 @@
+
+export const Profile = {
+
+    box: "m-0 grid place-items-center rounded-b-none py-8 px-4 text-center",
+    imag:" rounded-full w-64 h-64",
+    typ: "mx-auto box-border border-6 h-50px w-1200px bg-white rounded-xl shadow-2xl md:max-w-2xl md:py-2 "
+    
+} 
