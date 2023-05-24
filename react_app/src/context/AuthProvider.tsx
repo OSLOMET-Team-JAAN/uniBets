cb3ebe24bcc481e41b55a85b1a426b9285e33bae
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2023-05-24 14:06:31 +0200

    Updated with ref to idea source

--- react_app/src/context/AuthProvider.tsx
+++ react_app/src/context/AuthProvider.tsx
@@ -22,3 +22,5 @@ export const AuthProvider: FC<Props> = ({children}) => {
 }
 
 export default AuthContext;
+
+// https://github.com/gitdagray/react_protected_routes/blob/main/src/context/AuthProvider.js
